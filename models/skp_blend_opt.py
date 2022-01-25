d42a7fa2bce82be403ca8e1f0773a0e28bfbfe8a
Christian Mesh <christianmesh1@gmail.com>
2022-01-24 21:56:08 -0500

    Update models for lights/controls

--- models/skp_blend_opt.py
+++ models/skp_blend_opt.py
@@ -4,12 +4,12 @@ if bpy.context.selected_objects != []:
     for obj in bpy.context.selected_objects:
         if obj.type == 'MESH':
             print(obj.name)
-            bpy.context.scene.objects.active = obj
+            bpy.context.view_layer.objects.active = obj
             bpy.ops.object.editmode_toggle()
             bpy.ops.mesh.select_all(action='SELECT')
-            bpy.ops.mesh.faces_shade_flat()
+            #bpy.ops.mesh.faces_shade_flat()
             bpy.ops.mesh.remove_doubles()
-            bpy.ops.mesh.dissolve_limited(delimit={'SHARP'})
+            #bpy.ops.mesh.dissolve_limited(delimit={'SHARP'})
             bpy.ops.mesh.normals_make_consistent()
             bpy.ops.object.editmode_toggle()
 
